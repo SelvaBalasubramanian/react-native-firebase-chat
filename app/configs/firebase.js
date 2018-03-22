6b440683a3dc057d74b5830d36bc95a5fefaf8a9
SelvaBalasubramanian <selva6101996@gmail.com>
2018-03-22 21:24:16 +0530

    Modified Firebase

--- app/configs/firebase.js
+++ app/configs/firebase.js
@@ -6,3 +6,17 @@ export const firebaseConfig = {
   storageBucket: "",
   messagingSenderId: "708851137787"
 }
+
+// <script src="https://www.gstatic.com/firebasejs/4.11.0/firebase.js"></script>
+// <script>
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyD1Xpa5YE6Ni2awHKuMKBZrnbgMs4Od5EQ",
+//     authDomain: "cozy-b3af2.firebaseapp.com",
+//     databaseURL: "https://cozy-b3af2.firebaseio.com",
+//     projectId: "cozy-b3af2",
+//     storageBucket: "",
+//     messagingSenderId: "708851137787"
+//   };
+//   firebase.initializeApp(config);
+// </script>
